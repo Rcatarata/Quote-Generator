c8a10c75e1929ab919b74d0397537b25c8c32e06
Ruginn Catarata <Ruginn@Yip-Yip.attlocal.net>
2020-10-31 00:28:34 -0400

    fin js

--- script.js
+++ script.js
@@ -3,15 +3,44 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 // Get Quote from API
 async function getQuote() {
+    loading()
     const proxyUrl = 'http://cors-anywhere.herokuapp.com/'
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiURL);
         const data = await response.json();
-        console.log(data);
+        // author known or not
+        if (data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //Stop Loader, Show Quote 
+        complete();
     } catch(error){
         getQuote();
         
@@ -19,5 +48,15 @@ async function getQuote() {
 
 }
 
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`; 
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listener
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
 //On Load
 getQuote();
