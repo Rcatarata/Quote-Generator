4e8a736e2583f80bb497379fcd7e42d4c2caf4a0
Ruginn Catarata <Ruginn@Yip-Yip.attlocal.net>
2020-10-30 23:48:02 -0400

    updating js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
 
+// Get Quote from API
+async function getQuote() {
+    const proxyUrl = 'http://cors-anywhere.herokuapp.com/'
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl + apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        getQuote();
+        
+    }
+
+}
+
+//On Load
+getQuote();
